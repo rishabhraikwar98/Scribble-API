3cdd0669fedf253439cf8b856c90b8367aec6075
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2024-02-12 18:35:43 +0530

    added routes

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const { getMyProfile,updateMyProfile,deactivateMyProfile } = require("../controllers/profileController");
+const {
+  getMyProfile,
+  updateMyProfile,
+  deactivateMyProfile,
+  searchProfiles,
+  viewProfile,
+  followAccount,
+  unfollowAccount
+} = require("../controllers/profileController");
 const router = express.Router();
 router.route("/me").get(getMyProfile).patch(updateMyProfile);
 router.route("/me/deactivate").patch(deactivateMyProfile);
+router.route("/search").get(searchProfiles);
+router.route("/:id").get(viewProfile);
+router.route("/follow/:id").patch(followAccount)
+router.route("/unfollow/:id").patch(unfollowAccount)
+
 
 module.exports = router;
