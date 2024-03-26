e8e177c460e9e20a4699a83c18fd1a26d11bc2ca
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2024-03-26 19:50:15 +0530

    added post edit

--- controller/postController.js
+++ controller/postController.js
@@ -28,7 +28,7 @@ const editPost = async (req, res) => {
   try {
     const userId = req.user._id;
     const { postId } = req.params;
-    const { title } = req.body;
+    const { title,image } = req.body;
     const currentUser = await User.findById(userId);
     const currentPost = await Post.findById(postId);
     // Other user's post
@@ -39,7 +39,7 @@ const editPost = async (req, res) => {
       });
     }
     if (currentPost && currentUser.posts.includes(postId)) {
-      await Post.findByIdAndUpdate(postId, { title: title });
+      await Post.findByIdAndUpdate(postId, { title: title,image:image });
       res.status(200).json({
         status: "success",
         message: "Post updated!",
